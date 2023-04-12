b69a63f345af129c1dd7ff5c84dd4068f74894d2
MUKILTEO\1710136 <1710136@mukilteo.wednet.edu>
2023-04-12 10:20:03 -0700

    lol

--- Seventh Samurai/Assets/Tyler/Scripts/Tutorial.cs
+++ Seventh Samurai/Assets/Tyler/Scripts/Tutorial.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour
 {
     public GameObject TutorialCanvas;
-
+    public PlayerCombat PC;
+    public bool attackText;
     private void OnTriggerEnter(Collider other)
     {
         TutorialCanvas.SetActive(true);
+
+        if(attackText == true)
+        {
+            PC.enabled = true;
+        }
     }
 }
