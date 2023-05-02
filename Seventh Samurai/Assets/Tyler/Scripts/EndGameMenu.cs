4a7e799e3b825396044807066d7cfdef73530360
Blind0503 <93335443+Blind0503@users.noreply.github.com>
2023-05-01 22:06:51 -0700

    YOU WIN (*standing ovation*)

---
+++ Seventh Samurai/Assets/Tyler/Scripts/EndGameMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.SceneManagement;
+
+public class EndGameMenu : MonoBehaviour
+{
+    public int timeWait;
+    public GameObject endUI;
+    public GameObject fadeCanvas;
+    public Animator fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endUI.SetActive(false);
+        fadeCanvas.SetActive(false);
+        StartCoroutine("imDumb");
+    }
+
+    public IEnumerator imDumb()
+    {
+        yield return new WaitForSeconds(timeWait);
+        endUI.SetActive(true);
+    }
+
+    public IEnumerator waitforFade()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    public void returnToMenu()
+    {
+        fadeCanvas.SetActive(true);
+        //fade.Play("Fade");
+        StartCoroutine("waitforFade");
+    }
+}
